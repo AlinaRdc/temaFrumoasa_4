6a7a5b606bc9c9fa51a93582990a617065e7d141
colibry01 <109234266+AlinaRdc@users.noreply.github.com>
2022-08-18 17:47:14 +0300

    edit

--- tema_frumoasa.js
+++ tema_frumoasa.js
@@ -6,3 +6,14 @@ const interest = (amount * (interest_rate * 0.01)) / months;
 let payment = ((amount / months) + interest).toFixed(2); 
 document.querySelector('#payment').innerHTML = `Monthly Payment = ${payment}`
 }
+
+function depositLoan(){
+    const deposit = document.querySelector('#deposit').value;
+const interestRate = document.querySelector('#interestRate').value;
+const year = document.querySelector('#year').value;
+const interest = (deposit * (interestRate * 0.01)) / year;
+let payment = ((deposit / year) + interest).toFixed(2); 
+document.querySelector('#payment').innerHTML = `Yearly Payment = ${payment}`;
+let amount = (deposit + payment).toFixed(value);
+document.querySelector('#amount').innerHTML = `Amount Deposit = ${amount}`;
+}
